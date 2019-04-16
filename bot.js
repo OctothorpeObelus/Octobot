ad8988abde7a4eb9dd2cfe2d525158d28c449aab
Octo <OctothorpeObelus@users.noreply.github.com>
2019-04-16 12:30:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 const subnetting = require("./subnetting.js");
+const images = require("./images.js");
 
 const Jimp = require('jimp');
 
@@ -268,81 +269,22 @@ client.on("message", async message => {
             if (command == "trump") {
                 let trumpargs = message.content.split(" ")
                 //message.channel.send("[DEBUG] Args returned as {" + trumpargs + "}")
-
-                if (trumpargs.length < 2 | trumpargs.length > 2) {
-                    message.channel.send("You must mention a user");
-                    return;
-                } else if (trumpargs[1].substring(0, 4) == "http") {
-                    console.log("[CMD LOG] $trump, used url");
-                    const trumpurl = await message.channel.send("Processing...");
-                    let jimpsurl = ['images/trump_bkg.png', 'images/trump_picture_frame.png', trumpargs[1]]
-                    let jimpedurl = [Jimp.read(jimpsurl[0]), Jimp.read(jimpsurl[1]), Jimp.read(jimpsurl[2])]
-
-                    Promise.all(jimpedurl).then(function (data) {
-                        return Promise.all(jimpedurl);
-                    })
-                        .then(function (data) {
-                            data[2].resize(Jimp.AUTO, 760)
-                            data[0].composite(data[2], -(data[2].bitmap.width / 2) + 406, 94)
-                            data[0].composite(data[1], 0, 0)
-                            data[0].write("images/generated/trumped.png", function () {
-                                message.channel.send({
-                                    embed: {
-                                        color: 0xff0000,
-                                        author: {
-                                            name: message.author.username,
-                                            icon_url: message.author.avatarURL
-                                        },
-                                        title: "Generated Trump image",
-                                        timestamp: new Date()
-                                    }
-                                });
-                                message.channel.send("", { file: "images/generated/trumped.png" });
-                                trumpurl.delete();
-                            })
-                        })
-                        .catch(err => {
-                            throw err;
-                        })
-                } else if (trumpargs.length == 2 & message.mentions.users.first(1)[0].id != -1) {
-                    if (trumpargs[1].substring(0, 1) == "<") {
-                        console.log("[CMD LOG] $trump, used mention");
-                        const trump = await message.channel.send("Processing...");
-                        let jimps = ['images/trump_bkg.png', 'images/trump_picture_frame.png', message.mentions.users.first(1)[0].avatarURL]
-                        let jimped = [Jimp.read(jimps[0]), Jimp.read(jimps[1]), Jimp.read(jimps[2])]
-
-                        Promise.all(jimped).then(function (data) {
-                            return Promise.all(jimped);
-                        })
-                            .then(function (data) {
-                                data[2].resize(760, 760)
-                                data[0].composite(data[2], 55, 94)
-                                data[0].composite(data[1], 0, 0)
-                                data[0].write("images/generated/trumped.png", function () {
-                                    message.channel.send({
-                                        embed: {
-                                            color: 0xff0000,
-                                            author: {
-                                                name: message.author.username,
-                                                icon_url: message.author.avatarURL
-                                            },
-                                            title: "Generated Trump image",
-                                            timestamp: new Date()
-                                        }
-                                    });
-                                    message.channel.send({ file: 'images/generated/trumped.png' })
-                                    trump.delete();
-                                })
-
-                            })
-                            .catch(err => {
-                                throw err;
-                            })
-                    } else {
-                        findUser(trumpargs[1]);
-                        return;
-                    }
-                }
+                await images.trumped(trumpargs)
+                .then(
+                    message.channel.send({
+                        embed: {
+                            color: 0xff0000,
+                            author: {
+                                name: message.author.username,
+                                icon_url: message.author.avatarURL
+                            },
+                            title: "Generated Trump image",
+                            timestamp: new Date()
+                        }
+                    });
+                    message.channel.send({ file: 'images/generated/trumped.png' })
+                    trump.delete();
+                )
             }
 
             if (command == "vctime") {
