8b6963b3facfa16fadaa26557620080f887d5c12
Octo <OctothorpeObelus@users.noreply.github.com>
2019-04-18 11:34:32 -0400

    Update images.js

--- images.js
+++ images.js
@@ -1,6 +1,6 @@
 const Jimp = require('jimp');
 
-exports.trumped = function(trumpargs) {
+exports.trumped = function(trumpargs, message) {
     if (trumpargs.length < 2 | trumpargs.length > 2) {
         return "You must mention a user";
     } else if (trumpargs[1].substring(0, 4) == "http") {
@@ -37,4 +37,45 @@ exports.trumped = function(trumpargs) {
             return;
         }
     }
+}
+
+exports.mariouser = function(user, message) {
+    let jimps = ['images/mario_bkg.png', 'images/mario_frg.png',  (message.mentions.users.first(1)[0].id != -1)?message.mentions.users.first(1)[0].avatarURL:message.server.members.get("name", user).avatarURL]
+    let jimped = [Jimp.read(jimps[0]),Jimp.read(jimps[1]),Jimp.read(jimps[2])]
+    
+    Promise.all(jimped).then(function(data) {return Promise.all(jimped)})
+    .then(function(data) {
+        data[2].resize(256, 256)
+        data[0].composite(data[2], 55, 94)
+        data[0].composite(data[1], 0, 0)
+        data[0].write("images/generated/mario.png")
+    })
+    .catch(err => {console.log(err)})
+}
+
+exports.mariotext = function(text, message) {
+    let jimps = ['images/mario_bkg.png', 'images/mario_frg.png']
+    let jimped = [Jimp.read(jimps[0]),Jimp.read(jimps[1])]
+    
+    Promise.all(jimped).then(function(data) {return Promise.all(jimped)})
+    .then(function(data) {
+        data[2].resize(256, 256)
+        Jimp.loadFont(Jimp.FONT_SANS_16_BLACK).then(font => {
+            data[0].print(
+                font,
+                10,
+                10,
+                {
+                    text: text,
+                    alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                    alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+                },
+                64,
+                64
+            );
+        })
+        data[0].composite(data[1], 0, 0)
+        data[0].write("images/generated/mario.png")
+    })
+    .catch(err => {console.log(err)})
 }
